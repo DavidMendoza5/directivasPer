55495fade6b1bd85d7bd76e389bcbc806237449e
David <cruz-azul99@hotmail.com>
2021-01-21 11:30:54 -0600

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="badge bg-secondary">
+      <!-- Enviamos un argumento y un modificador a la directiva personalizada -->
+      <!-- v-decorar:grande.negrilla="'helvetica'" -->
+      <span v-decorar:grande.negrilla="{familia: 'helvetica', color: 'red'}">Aprende Vue</span>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
+  // Se pueden definir directivas locales con la palabra directives y se le manda un objeto que contenga las directivas personalizadas
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,25 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// Creación de una directiva personalizada llamada decorar (v-decorar), el primer argumento es el elemento que recibe, el segundo es algún parámetro
+// que se envíe
+Vue.directive('decorar', {
+  bind(el, binding) {
+    el.style.fontFamily = binding.value.familia
+    el.style.color = binding.value.color
+
+    // Revisamos si se mandó algún argumento
+    if(binding.arg === 'grande') {
+      el.style.fontSize = '50px'
+    }
+    
+    // Revisamos si se mandó algún modificador
+    if(binding.modifiers['negrilla']) {
+      el.style.fontWeight = 'bold'
+    }
+  }
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
